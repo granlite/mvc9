354002c631eceb8ecd7899dad6581faf1cec616e
granlite <65130690+granlite@users.noreply.github.com>
2020-09-15 23:51:09 +0400

    Add files via upload

---
+++ mvc9/Models/Album.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace muscshop.Models
+{
+    public class Album
+    {
+        Required(ErrorMessage = "enter the title")]
+        public string Title { get; set; }
+        [Range(1, 300, ErrorMessage = "enter price: 1 to 300")]
+        public double Price { get; set; }
+        [Display(Name = "Photo URL")]
+        public string AlbumUrl { get; set; }
+        [Required(ErrorMessage = "ReleaseYear is required")]
+        [Display(Name = "Release Year")]
+        [Range(1960, 2020, ErrorMessage = "enter year: 1960 to 2020")]
+        public int ReleaseYear { get; set; }
+        public Artist Artist { get; set; }
+        public Genre Genre { get; set; }
+        public int AlbumId { get; set; }
+        [MaxLength(100, ErrorMessage = "Max 100 symbols")]
+        public string Description { get; set; } = "albumDescription";
+        public int ArtistId { get; set; }
+        public int GenreId { get; set; }
+        public Album()
+        {
+            AlbumUrl = "~/App_Files/Images/default.png";
+        }
+    }
+}
---
+++ mvc9/filters/AutorizeUserAttribute.cs
@@ -0,0 +1,43 @@
+﻿using muscshop.Context;
+using muscshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace muscshop.filters
+{
+    public class AutorizeUserAttribute : AuthorizeAttribute
+    {
+        private readonly string[] allowRoles;
+        public AutorizeUserAttribute(params string [] roles)
+        {
+            this.allowRoles = roles;
+        }
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            var autorized = false;
+            var user = (User)filterContext.HttpContext.Session["User"];
+            if(user != null)
+            {
+                using (var _storeContext = new StoreContext())
+                {
+                    var roleNames = user.Roles.Select(x => x.RoleName);
+                    autorized = roleNames.Any(item => allowRoles.ToList().Contains(item));
+                }
+            }
+            if(!autorized)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary
+                    {
+                        {"Controller", "Account" },
+                        {"Action", "Login" }
+                    }
+                );
+            }
+        }
+    }
+}
